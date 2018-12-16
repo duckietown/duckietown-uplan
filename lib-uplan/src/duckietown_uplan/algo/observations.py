ff7a9dbea6fbf8f0bcefff538048f6950ba10b08
amuni3 <aneri2794@gmail.com>
2018-12-16 15:00:59 +0100

    Observation Model implemented for uncertainty probabilities

--- lib-uplan/src/duckietown_uplan/algo/observations.py
+++ lib-uplan/src/duckietown_uplan/algo/observations.py
@@ -5,22 +5,44 @@ __all__ = [
     'ObservationModel',
 ]
 
+import collections
+observation_memory_length = 15
+initial_obstacle_prob = 0.2
+discount_factor = 0.8
 
 class ObservationModel(object):
     def __init__(self, map):
-        raise Exception("Observation Model __init__ not implemented yet")
+        self.graph = map
+        self.curr_uncertainty_values = dict()
+        for key in self.graph.nodes:
+            self.curr_uncertainty_values[key] = 0.2
+        # Queue of nodes observed in the recent past
+        self.uncertainty_history = collections.deque(maxlen=observation_memory_length)
 
     def reset_obstacles_uncertainity(self):
-        raise Exception("Observation Model reset_obstacles_uncertainity not implemented yet")
+        for key in curr_uncertainty_values.keys():
+            curr_uncertainty_values[key] = initial_obstacle_prob
 
-    def get_observations_from_frame(self):
-        raise Exception("Observation Model get_observations_from_frame not implemented yet")
+    def get_uncertainty_from_node(self, node):
+        return self.curr_uncertainty_values[node]
 
     def update_obstacles_uncertainity(self):
-        raise Exception("Observation Model update_obstacles_uncertainity not implemented yet")
+        from duckietown_uplan.environment.duckie import Duckie
+        for key, value in Duckie.get_current_observations(): ## Returns a dict()
+            self.curr_uncertainty_values[key] = value
+        for n in self.uncertainty_history:
+            if n in Duckie.get_current_observations():
+                continue
+            #reduce it if its higher than the initial obstacle prob and increase otherwise
+            if self.curr_uncertainty_values[n] > initial_obstacle_prob:
+                self.curr_uncertainty_values[n] = self.curr_uncertainty_values[n] * discount_factor
+            elif self.curr_uncertainty_values[n] < initial_obstacle_prob:
+                self.curr_uncertainty_values[n] = self.curr_uncertainty_values[n] + (initial_obstacle_prob / observation_memory_length)
+            else:
+                continue
 
-    def update_observations_history(self):
-        raise Exception("Observation Model update_observations_history not implemented yet")
+        def update_observations_history(self, node):
+            uncertainty_history.append(node)
 
-    def forget_observations_history(self):
-        raise Exception("Observation Model forget_observations_history not implemented yet")
+        def forget_observations_history(self):
+            raise Exception("Observation Model forget_observations_history not implemented yet")
