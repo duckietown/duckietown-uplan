4d511cf71f629c8cac9daba723efad0ebc2f0879
Ahmad Roumie <43572412+aroumie1997@users.noreply.github.com>
2018-12-26 14:10:06 +0200

    Update __init__.py

--- lib-uplan/src/duckietown_uplan_tests/__init__.py
+++ lib-uplan/src/duckietown_uplan_tests/__init__.py
@@ -3,7 +3,7 @@
 # add an import for each test file in this directory
 #from .test1 import *
 # from .test2 import *
-from test_sim import *
+from .test_sim import *
 
 def jobs_comptests(context):
     """ Hook for comptests. No need to modify."""
